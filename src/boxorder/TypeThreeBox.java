ac8806e99e2d4705f0204851612002fa9a73559d
Zak Dowsett <z.dowsett@outlook.com>
2016-11-25 20:50:12 +0000

    added quantity and contents

--- src/boxorder/TypeThreeBox.java
+++ src/boxorder/TypeThreeBox.java
@@ -2,24 +2,27 @@ package boxorder;
 
 public class TypeThreeBox extends SuperBox {
     
+    // Constructors
     public TypeThreeBox(){
     }
     
-    public TypeThreeBox(double w, double h, double l, int grd, boolean tops){
-        super(w, h, l, grd, tops);
+    public TypeThreeBox(double w, double h, double l, int grd, boolean tops, int quant){
+        super(w, h, l, grd, tops, quant);
     }
     
     
+    // Methods
     public double sizeCalculator() {
         return super.sizeCalculator();
     }
     
+    // Calculate cost of boxes
     public double costCalculator() {
         double gradeCost = 0;
         double topsCost = 0;
         double colourCost = 0;
         
-        switch (grade) {
+        switch (grade) { // Calculate initial costs of boxes by size and grade
             case 2: gradeCost = size * 0.6;
                     break;
             case 3: gradeCost = size * 0.72;
@@ -30,12 +33,12 @@ public class TypeThreeBox extends SuperBox {
                     break;
         }
       
-        if(sealableTops)
+        if(sealableTops) // Calculate additional cost of sealable box if chosen
             topsCost = gradeCost * 0.08;
         
-        colourCost = gradeCost * 0.16;
+        colourCost = gradeCost * 0.16;  // Calculate cost of having additional colours
         
-        cost = gradeCost + topsCost + colourCost;
+        cost = (gradeCost + topsCost + colourCost) * quantity; // Calculate final cost
         return cost;            
     }
 }
